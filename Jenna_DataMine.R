70c15ca738c55e1778c27ff6256a5bf9c662b2de
jennapl <jennybear11037@gmail.com>
2022-06-27 14:41:02 -0500

    Column issue solution (top)

--- Jenna_DataMine.R
+++ Jenna_DataMine.R
@@ -1,10 +1,42 @@
+#Libraries -----
 library(dplyr)
 library(readr)
 library(tidyverse)
 library(ggplot2)
 
-#Read in Data
-scp <- read.csv("C:/Users/jplus/OneDrive/Documents/DataLab/ER_Usage/ER/scp_data")
+################################################################################
+# Diag Column -----
+################################################################################
+# SOLUTION FOR COLUMN READING PROBLEM
+
+# 1. Read google sheet of ICD-10 codes into R
+icd <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/14m7RGYPh17lv1EQ5Tv9gbpj1kvzlVmeUQRZs0MkUyUw/edit#gid=0")
+
+# 2. Turn dataframe into vector
+icd <- as.vector(unlist(icd$'ICD-10'))
+
+#3. Read in data
+scp <- read.csv("C:/Users/jplus/Downloads/scp_data")
+
+#4. Make diag into one columns with multiple rows per patient
+scp <- scp %>% pivot_longer(starts_with("Diag"))
+
+#5. Filter by patient ID to see what patients have a ICD 10 code in the vector
+scp <- scp %>% group_by(Patient_ID) %>% filter(value %in% icd)
+
+#Filter by unique ids (OPTIONAL)
+#ids <- pull(scp, Patient_ID) %>% unique(ids)
+
+#6. Make Diag 1-18 cols again, n/a unless diag is in ICD vector
+# NOTE: Rows are of only patients with at least one ICD 10 code in vector, 
+# so patients with none are removed. Columns with values that are not in 
+# vector are changed to N/A as a result of the sort and pivot wider.
+scp <- scp %>% pivot_wider(names_from = name, values_from = value)
+
+################################################################################
+################################################################################
+################################################################################
+#General Brainstorming-----
 
 #Remove random count row that generated when opened CSV
 scp <- select( scp, -X)
@@ -26,12 +58,6 @@ scp %>%
 
 # Looking at Non-Emergent ICD10
 
-#Read google sheet into R
-icd <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/14m7RGYPh17lv1EQ5Tv9gbpj1kvzlVmeUQRZs0MkUyUw/edit#gid=0")
-
-#Turn dataframe into vector
-icd <- as.vector(unlist(ne_icd_10$'ICD-10'))
-
 #Attempt to select diag columns only, ERROR
 test <- scp %>% filter(select(scp, contains("Diag")) %in% icd) %>% tally()
 
@@ -44,6 +70,23 @@ test <- scp %>% filter(select(scp, contains("Diag")) %in% icd) %>% tally()
                                           Diag9 %in% icd | Diag10 %in% icd |Diag11 %in% icd | Diag12 %in% icd | 
                                           Diag13 %in% icd | Diag14 %in% icd |Diag15 %in% icd | Diag16 %in% icd | 
                                           Diag17 %in% icd | Diag18 %in% icd, "TRUE", "FALSE"))
+  
+#SOLUTION is now at top
+
+  #1. Read in data
+  scp <- read.csv("C:/Users/jplus/Downloads/scp_data")
+  
+  #2. Make diag into one columns with multiple rows per patient
+  scp <- scp %>% pivot_longer(starts_with("Diag"))
+  
+  #3. Filter by patient ID to see what patients have a ICD 10 code in the vector
+ scp <- scp %>% group_by(Patient_ID) %>% filter(value %in% icd)
+ 
+ #4. Filter by unique ids (OPTIONAL)
+ #ids <- pull(scp, Patient_ID) %>% unique(ids)
+ 
+ #Make Diag cols again, n/a unless diag is in ICD vector
+ scp <- scp %>% pivot_wider(names_from = name, values_from = value)
 
 
 
