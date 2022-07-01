26849a22866e69ef7a014c8628df2e3bae0e5cc1
jennapl <jennybear11037@gmail.com>
2022-07-01 13:36:41 -0500

    Fixed Dashboard double issue and refined skeleton

--- ER_Overuse_Dash/app.R
+++ ER_Overuse_Dash/app.R
@@ -8,60 +8,57 @@
 
 library(shiny)
 library(bslib)
+library(markdown)
+library(shinydashboard)
 
 ################################################################################
 # UI ------
 ################################################################################
-ui <- fluidPage(
+
   
   # Header
   header <- dashboardHeader(
     title = "Investigating ER Overuse", 
     titleWidth = 300
-    ), 
+    )
   
   #Side Navigation Bar
   sidebar <- dashboardSidebar(
     sidebarMenu(
       HTML(paste0(
-        "<br>",
-        #TO DO: Photo
-        #"<a href='https://mobile.twitter.com/sewaneedatalab' target='_blank'><img style = 'display: block; margin-left: auto; margin-right: auto;' src='US-NationalParkService-Logo.svg' width = '186'></a>",
-        "<br>",
-        "<br>",
-        "<br>",
-        "<br>",
-        #"<p style = 'text-align: center;'><small><a href='https://mobile.twitter.com/sewaneedatalab' target='_blank'>NPS logo disclaimer</a></small></p>",
         "<br>"
       )),
+      HTML(paste0(
+        "<a href='datalab_logo.jpg' target='_blank'><img style = 'display: block; margin-left: auto; margin-right: auto;' src='datalab_logo.jpg' width = '186'></a>",
+        "<br>"
+        )),
+      
     # Tabs
     menuItem("Overview", tabName = "Overview", icon = icon("home")),
     menuItem("ER Overuse", tabName = "ER_Overuse", icon = icon("bar-chart-o")),
     menuItem("Map", tabName = "Map", icon = icon("map")),
     menuItem("Explore", tabName = "explore", icon = icon("table")),
     menuItem("Conclusion", tabName = "con", icon = icon("tasks"))
-    )),
+    ))
   
   # Body
   body <- dashboardBody(
-    
-    # tabItems(
-    #   tabItem(tabName = "Overview",
-    #           includeMarkdown("www/overview.Rmd")
-    #   ),
+    tabItems(
+      #Adding Rmarkdown welcome page
+        tabItem(tabName = "Overview",
+                includeMarkdown("www/overview.Rmd")),
+        tabItem(tabName = "ER_Overuse",
+                includeMarkdown("www/er_overuse.Rmd"))
+        )
+  )
     #Testing to see if using HTML would be good or if we should just use RMARK
-    # )
-  ),
+     
   
   # Compile UI
-  dashboardPage(header, sidebar, body, 
-                skin = "red"),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           tabPanel("Test")
-        )
-    )
+ui <- dashboardPage(header, sidebar, body, 
+                    skin = "red")
+      
+    
 
 
 
@@ -72,9 +69,6 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-    })
    
 }
 
