d61a65ed13fe00ebaa52f2cbac0d3b82b6a488a7
Kenedi Clinton <clintkd0@sewanee.edu>
2022-07-08 13:54:07 -0500

    Looking at dashboard

--- KenBrainStorm.R
+++ KenBrainStorm.R
@@ -2,15 +2,21 @@ library(readr)
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
+library(gsheet)
 
+##########################################################################################################
 ##########################################################################################################
 
 ## Read in Hospital discharge data
-scp <- read_csv("Downloads/scp_data")
+scp <- read_csv("DataLab/ER/scp_data2")
+scp %>%  view
+
+  # Rename weird column
 scp <- rename(scp, visit = ...1)
 
 scp <- scp %>% filter(ER_Record_Flag == "Y")
 
+##########################################################################################################
 ##########################################################################################################
 ##########################################################################################################
 
@@ -30,12 +36,14 @@ acs <- as.vector(unlist(acs$'ICD_10_code'))
 acs <- paste0( acs, collapse = "|^" ) 
 
 # NOTES for why I did this ^ :
+
 # "collapse" squishes the vector into "___ or ___ or ___.." statement that grepl() can read. 
 # "^" tells R to search for anything that IS or STARTS WITH the value that it precedes. 
 
 acs <- paste0("^", acs) # This adds the "^" to the first value, b/c it didn't do it in 
 # the last code for some reason.
 
+##########################################################################################################
 ##########################################################################################################
 
 ## NON EMERGENT codes vector:
@@ -48,6 +56,7 @@ non_emerg <- paste0( non_emerg, collapse = "|^" )
 
 non_emerg <- paste0("^", non_emerg)
 
+##########################################################################################################
 ##########################################################################################################
 
 ## MENTAL HEALTH codes vector:
@@ -76,6 +85,7 @@ mental <- paste0( mental, collapse = "|^" )
 mental <- paste0("^", mental)
 
 ##########################################################################################################
+##########################################################################################################
 
 ## SUBSTANCE ABUSE codes vector
 sub_abuse <- c()
@@ -88,6 +98,7 @@ for(code in 11:19){
 sub_abuse <- paste0( sub_abuse, collapse = "|^" )
 sub_abuse <- paste0("^", sub_abuse)
 
+##########################################################################################################
 ##########################################################################################################
 
 ##DENTAL codes vector:
@@ -103,48 +114,78 @@ dental <- paste0("^", dental)
 
 ##########################################################################################################
 ##########################################################################################################
+
+# Run the following for an updated 'scp' data frame that has:
+
+# New column "county" for county names.
+# Vectors for each county
+grundy_zip <- c("37301",
+                "37305",
+                "37313",
+                "37339",
+                "37356",
+                "37365",
+                "37366",
+                "37387")
+franklin_zip <- c("37375",
+                  "37383")
+marion_zip <- c("37397",
+                "37374")
 ##########################################################################################################
-
-  # What percentage of all visits to the ER had a primary diagnosis of ACSC?
-scp %>% 
-  group_by(acs_primary) %>%
-  tally %>% 
-  mutate(total = sum(n)) %>%
-  group_by(acs_primary, n, total) %>% 
-  summarise(perc = n/total*100)
-# The column name in group_by() can be replaced to see trends for other conditions
-# (mental, dental, substance abuse, etc.)
-
-
 ##########################################################################################################
-
-# New columns that indicate whether or not the primary diagnosis was ACSC, mental, dental, etc.,
-  # New column called 'Age_Group' that sorts patients into groups by decade,
-  # Removes N/As for Patient_Sex.
 
 scp <- scp %>%
+  # Removes N/As for Patient_Sex
   drop_na(Patient_Sex) %>% 
-  mutate(acs_primary = grepl(acs, Diag1), 
+  # New columns that indicate whether or not the primary diagnosis was ACSC, mental, dental, etc.,
+  mutate(acs_primary = grepl(acs, Diag1),
          nonemerg_primary = grepl(non_emerg, Diag1),
          mental_primary = grepl(mental, Diag1),
          subabuse_primary = grepl(sub_abuse, Diag1),
          dental_primary = grepl(dental, Diag1),
+         # New column called 'Age_Group' that sorts patients into groups by decade
          age_group = cut(Age,
                          breaks = 10,
                          labels = c('0-9', '10-19', '20-29', '30-39', '40-49',
-                                    '50-59', '60-69', '70-79', '80-89', '90-99')))
+                                    '50-59', '60-69', '70-79', '80-89', '90-99')),
+         # New column "county" for county names.
+         
+         county = ifelse(Patient_Zip %in% grundy_zip, 
+                         "Grundy", 
+                         ifelse( Patient_Zip %in% franklin_zip,
+                                 "Franklin",
+                                 ifelse(Patient_Zip %in% marion_zip,
+                                        "Marion",
+                                        "NULL"))))
+
+# Run the following to create a new column "county_total" that will track
+# the total number of ER visits in each county.
+# Useful for calculating percentages etc.; makes code more easily reproducible.
+
+# Variable showing total # of ER visits for each county.
+county_visits <- scp %>%
+  filter(ER_Record_Flag == 'Y') %>%
+  group_by(county) %>%
+  tally()
+
+# Join 'county_visits' with 'scp' to make a new column
+scp <- inner_join(scp, county_visits, by = 'county') %>%
+  dplyr::rename(county_total= 'n')
+
+# Next, run the following to add a new column that says Race in characters rather
+# than in values:
+
+# Make a vector of names (in order, 1 = white, 2 = Black, etc.)
+races_vec <- c("White", "Black", "Native American") 
+
+# Create the new column called 'Race_Chr'
+scp <- scp %>% 
+  mutate(Race_Chr = ifelse(Race == 9, "Unkown", races_vec[Race]))
 ##########################################################################################################
-## BRAINSTORM
-
-  # How many ER Flags are Yes or No
-table(scp$ER_Record_Flag) 
-
-  # How many Type of ER visits are there
-ER  <- scp_data %>% 
-  select(Type_ER_Visit)
-table(ER)
-
 ##########################################################################################################
+##########################################################################################################
+
+# Percentages
   
   # What percentage of all visits to the ER had a primary diagnosis of ACSC?
   perc_acsc <- scp %>% 
@@ -153,12 +194,11 @@ table(ER)
   mutate(total = sum(n)) %>%
   group_by(acs_primary, n, total) %>% 
   summarise(perc = n/total*100)
-  
 
-  # What percentage of all visits to the ER had a primary diagnosis of non emergencies? 
     # The column name in group_by() can be replaced to see trends for other conditions
     # (non emergent, mental, dental, substance abuse, etc.)
-    # example below shows non emergent trends
+
+    # Shows non emergent trends
 perc_nonemerg <- scp %>% 
   group_by(nonemerg_primary) %>%
   tally %>% 
@@ -190,6 +230,23 @@ perc_subabuse <- scp %>%
   group_by(subabuse_primary, n, total) %>% 
   summarise(perc = n/total*100)
 
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+
+## BRAINSTORM
+
+# How many ER Flags are Yes or No
+table(scp$ER_Record_Flag) 
+
+# How many Type of ER visits are there
+ER  <- scp %>% 
+  select(Type_ER_Visit)
+table(ER)
+
+##########################################################################################################
+##########################################################################################################
+
 # What are the types of ACS conditions that we are seeing in the emergency room? 
   # Which ones are we seeing most often?
 acs_types <- scp %>%
@@ -218,12 +275,6 @@ acs_zip_hospital <- scp %>%
   filter(acs_primary == TRUE) %>% 
   group_by(Patient_Zip, JARID) %>% 
   tally()
-
-  # What hospitals are people from Grundy zip codes going to most frequently?
-hospital_grundy <- scp %>% 
-  filter(Patient_Zip %in% c('A','B','C','D','F','K','L','N')) %>% 
-  group_by(JARID, Patient_Zip) %>% 
-  tally()
     
     # For which conditions?
 acs_grundy_a <- scp %>% 
@@ -231,7 +282,32 @@ acs_grundy_a <- scp %>%
   group_by(JARID, Diag1) %>% 
   tally()
 
-  # What does the number of UTI look like in different counties for different genders?
+############9########################################################################################################################################################################################################
+####################################################################################################################################################################################################################
+####################################################################################################################################################################################################################
+
+##Top ICD 5 codes for each county, zip, and insurance
+  # We already have a column with 'county_visits' so that will be used for percentages as our sum
+    # What are the top ICD 10 codes of a certain county? Percentage wise
   
-  
+  # What are the top ICD 10 codes for Marion county?
+marion_icd_perc <- scp %>% 
+  filter(county== 'Marion', ER_Record_Flag == 'Y') %>% 
+  group_by(Diag1, county_total) %>% 
+  tally() %>% 
+  arrange(desc(n))%>% 
+  summarise(perc = n/county_total*100) %>% 
+  arrange(desc(perc)) %>% 
+  head(5)
+
+  # Make a graph for this data
+ggplot(data= marion_icd_perc) +
+  geom_col(aes(x= Diag1, y= perc, fill=Diag1))+
+  theme_light(base_size = 18)+
+  scale_fill_manual(values= c('#fdcc8a',
+    '#a1dab4',
+    '#41b6c4',
+    '#2c7fb8',
+    '#253494'))
+
 
