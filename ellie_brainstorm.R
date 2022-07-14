fce3036c3fa639ebd9948543e5dca1e63bf0cffe
Ellie Davis <ellie.davis01@gmail.com>
2022-07-14 10:10:21 -0500

    leaflet work

--- ellie_brainstorm.R
+++ ellie_brainstorm.R
@@ -80,9 +80,6 @@ top_ten <- scp %>%
   arrange(desc(n)) %>% 
   head(10)
 
-bigpic_hosp <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1JzmcNpV1bYoW3N6sg7bYVgL33LERLbS__CtSYB1_bX4/edit?usp=sharing")
-
-
 bigpic <- scp %>% 
   group_by(Patient_Zip, acs_primary, nonemerg_primary) %>% 
   tally %>% 
@@ -101,8 +98,15 @@ bigpic <- scp %>%
   
 bigpic_zip <- inner_join(bigpic, zipcodes, by = "Patient_Zip")
 
+bigpic_hosp <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1JzmcNpV1bYoW3N6sg7bYVgL33LERLbS__CtSYB1_bX4/edit?usp=sharing")
+
+doctor <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1ZqRk8NK4qp43bA30Q0VyBEWVX9Z_Zd_bgk6_Mt_dDW8/edit?usp=sharing")
+  
+urgent_care <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1_tTNbY0YQKAVF51rQcULq0qInGoMIz9thJieHbbfXfs/edit?usp=sharing")
+
 palette <- colorNumeric(palette = c('#0571b0','#92c5de',  '#f7f7f7', '#f4a582', '#ca0020'),
                         domain = bigpic_zip$overuse_perc)
+
 
 leaflet() %>% 
   addTiles() %>% 
@@ -113,20 +117,77 @@ leaflet() %>%
               fillColor = palette(bigpic_zip$overuse_perc),
               weight = 0.5,
               smoothFactor = 0.25,
-              opacity = 0.75,
+              opacity = 0.25,
               fillOpacity = .75,
               highlightOptions = highlightOptions(color = "white",
                                                   weight = 1.5,
-                                                  opacity = 1.0,
-                                                  bringToFront = TRUE),
-              label = paste0("Zip: ", unique(bigpic_zip$Patient_Zip))) %>% 
+                                                  opacity = 1.0),
+              label = paste0("Zip: ", unique(bigpic_zip$Patient_Zip), 
+                             ' | ER overuse = ', 
+                             round(bigpic_zip$overuse_perc, 1),'%')) %>% 
+  
+  #label = ~lapply(paste0("Zip code: ", bigpic_zip$Patient_Zip, ",",
+  #               "<br/>",
+  #               paste(round(get(bigpic_zip$overuse_perc), 2)), "%"), HTML)) %>% 
+  addCircleMarkers(lat = doctor$lat,
+                   lng = doctor$lng,
+                   radius =2,
+                   color = "green",
+                   opacity = 0.75) %>%
+  addCircleMarkers(lat = urgent_care$lat,
+                   lng = urgent_care$lng,
+                   radius = 2,
+                   color = "red",
+                   opacity = 0.75) %>%
   addLegend("bottomright",
             pal = palette,
             values = bigpic_zip$overuse_perc,
             opacity = 0.75,
-            title = "% ER Overuse")
-  
+            title = "% Overuse")
 
 
 
+
+
+
+
+
+################################
+
+test <- scp %>%
+  filter(insurance == c("TennCare", "MediCare", "Self Paid", "Commercial")) %>% 
+  group_by(insurance, acs_primary, nonemerg_primary) %>% 
+  tally %>% 
+  ungroup() %>% 
+  group_by(insurance) %>% 
+  mutate(total = sum(n)) %>%
+  summarise(percentage = n/total*100, across(everything())) %>% 
+  mutate(type = case_when(!acs_primary & !nonemerg_primary ~ "Appropriate Use", 
+          acs_primary ~ "ACS Conditions",
+          nonemerg_primary ~ "Nonemergent Conditions")) %>% 
+  mutate(type2 = ifelse(type == "Appropriate Use", "Appropriate Use", "Overuse")) %>% 
+  filter(type2 != "Appropriate Use")
+# Dashboard graph                    
+ggplot(data = test, aes(x = insurance, y = percentage/100, fill = type)) +
+  geom_col(position = 'dodge') +
+  scale_fill_manual(name = ' ',
+                    values=c('#41b6c4',
+                             '#253494')) +
+  scale_y_continuous(labels = scales::percent) + 
+  labs(x = ' ', y = 'Percentage of Visits',
+       title = 'Severity of Overuse by Insurance Type') +
+  theme_light(base_size = 18) +
+  theme(legend.position = 'bottom')
+
+# poster graph
+ggplot(data = test, aes(x = insurance, y = percentage/100, fill = type)) +
+  geom_col(position = 'dodge') +
+  scale_fill_manual(name = ' ',
+                    values=c('#af89fa',
+                             '#7fbf7b')) +
+  scale_y_continuous(labels = scales::percent) + 
+  labs(x = ' ', y = 'Percentage of Visits',
+       title = 'Severity of Overuse by Insurance Type') +
+  theme_light(base_size = 12) +
+  theme(legend.position = 'bottom')
 
